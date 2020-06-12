1778d9f79c6f2d34b30a353d1443ff4f7431011d
cannedfish <hehaowengy@sina.com>
2020-06-13 00:22:32 +0800

    初步实现了时间的迭代变化，具体为Simulator文件下的Timer类

--- src/Simulator.java
+++ src/Simulator.java
@@ -1,4 +1,8 @@
-public class Simulator {
+import java.lang.*;
+import Object.*;
+public class Simulator
+{
+    Timer global=new Timer();
     //Under Construction
 
     /*          To-Do List
@@ -7,8 +11,46 @@ public class Simulator {
         3、实现数据统计
         4、Java-IO到文件以及可能的GUI设计
                                                     ——何浩文
-
      */
+}
+class Timer extends Thread{
+    int currentTime;                      //时间放大比 1:630,单位为秒，判断依据为21/2
+    int secs;
+    int mins;
+    int hours;
+
+    Timer(){
+        currentTime=0;
+        secs=0;
+        mins=0;
+        hours=0;
+    }
+
+    public void run(){
+        try {
+            while(true) {
+                secs=currentTime%60;
+                mins=currentTime/60;
+                hours=mins/60;
+                if(currentTime!=0)mins%=mins;
+                if(hours*60*60+mins*60+secs>=42497)break;              //(18-8.5)*60*60+((24+21+62+21+24+22)/1.4+7*2)
+                else {
+                    Timer.sleep(1000);
+                    currentTime+=630;
+                }
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+    int[] returnCurrentTime(){
+        int secs=currentTime%60;
+        int mins=currentTime/60;
+        int hours=mins/60;
+        mins%=mins;
+        return new int[]{hours, mins, secs};
+    }
 }
 
 
